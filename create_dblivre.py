27b29585742e14a833aa0417b9ac5ec198c52da0
Rabiaa123 <r.amarire@gmail.com>
2025-03-20 09:19:10 +0100

    Create create_dblivre.py

---
+++ create_dblivre.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+connection = sqlite3.connect('databaselivre.db')
+
+with open('schemalivre.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)",(1, 'JAVA', 'Victor', 2024, 10))
+cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)",(2, 'HTML', 'Laurent', 2023, 5))
+cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)",(3, 'SQL', 'Nathalie', 2025, 20))
+cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)",(4, 'PYTHON', 'Rabiaa', 2020, 15))
+
+connection.commit()
+connection.close()
