c72fc9be2cd8bd51039289b50d5ad0c904d17d7a
Rabiaa123 <r.amarire@gmail.com>
2025-03-20 08:38:52 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session
- from flask import render_template
- from flask import json
- from urllib.request import urlopen
- from werkzeug.utils import secure_filename
- import sqlite3
+from flask import render_template
+from flask import json
+from urllib.request import urlopen
+from werkzeug.utils import secure_filename
+import sqlite3
  
  app = Flask(__name__)                                                                                                                  
  app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions
