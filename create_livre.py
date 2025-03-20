0f7211e2799703d33ee4dc2b206dad5c2f8613f8
Rabiaa123 <r.amarire@gmail.com>
2025-03-20 09:08:12 +0100

    Update create_livre.py

--- create_livre.py
+++ create_livre.py
@@ -3,22 +3,13 @@ import sqlite3
 try:
     # Connexion à la base de données
     connection = sqlite3.connect('databaselivre.db')
+    connection.execute("PRAGMA foreign_keys = ON;")  # Activer les clés étrangères
 
-    # Exécution du script SQL pour créer la table
+    # Exécution du script SQL
     with open('schemalivre.sql') as f:
         connection.executescript(f.read())
 
-    # Création d'un curseur
-    cur = connection.cursor()
-
-    # Insertion des données
-    cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)", (1, 'JAVA', 'Victor', 2024, 10))
-    cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)", (2, 'HTML', 'Laurent', 2023, 5))
-    cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)", (3, 'SQL', 'Nathalie', 2025, 20))
-    cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)", (4, 'PYTHON', 'Rabiaa', 2020, 15))
-
-    # Validation des changements
-    connection.commit()
+    print("Tables créées avec succès.")
 
 except sqlite3.Error as e:
     print(f"Une erreur SQLite s'est produite : {e}")
